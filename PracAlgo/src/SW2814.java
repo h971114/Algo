3909c62ad4959737db8829e9301dc33223fb909e
현수진(Sujin Hyeon) <h971114@likelion.org>
2021-06-06 18:35:00 +0900

    sw2814

---
+++ PracAlgo/src/SW2814.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SW2814 {
+	static int N,M,T,x,y,ans;
+	static boolean map[][],visit[];
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		for(int tc=1;tc<=T;tc++) {
+			StringTokenizer st = new StringTokenizer (br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			map = new boolean[N+1][N+1];
+			visit = new boolean[N+1];
+			if (M==0) {
+				ans=1;
+				System.out.println("#"+tc+" "+ans);
+				continue;
+			}
+			for(int i=1;i<=M;i++) {
+				 st = new StringTokenizer (br.readLine());
+				 x = Integer.parseInt(st.nextToken());
+				 y = Integer.parseInt(st.nextToken());
+				 map[x][y] = true;
+				 map[y][x] = true;
+			}
+			for(int i=1;i<=N;i++) {
+				visit[i] = true;
+				find(i,1);
+				visit[i] = false;
+			}
+			System.out.println("#"+tc+" "+ans);
+		}
+
+	}
+	private static void find(int start, int cnt) {
+		if(ans<cnt)ans = cnt;
+		for(int i=1;i<=N;i++) {
+			if(map[start][i]&&!visit[i]) {
+				visit[i]=true;
+				find(i,cnt+1);
+				visit[i]=false;
+			}
+		}
+		
+	}
+
+}
